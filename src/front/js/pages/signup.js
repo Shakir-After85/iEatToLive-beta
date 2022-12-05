30bde60ce096321a20f59a2135def75fcb6f0410
Natucherypu1 <nathalie.chery@gmail.com>
2022-12-05 20:37:32 +0000

    added login and signup

---
+++ src/front/js/pages/signup.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+export const SignUp = () => {
+  const navigate = useNavigate();
+  function handleSubmit() {
+    navigate("/login");
+  }
+  return (
+    <div className="container w-50 pt-5">
+      <form onSubmit={handleSubmit} className="bg-white mt-5 p-5">
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            First Name
+          </label>
+          <input type="text" aria-label="First name" class="form-control" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Last Name
+          </label>
+          <input type="text" aria-label="Last name" class="form-control" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Keep me signed in
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};

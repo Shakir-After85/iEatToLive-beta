30566aecf94036eb3be744261b7753c786691483
Shakir-After85 <shakir.tamar@gmail.com>
2022-12-29 22:12:57 +0000

    popdb fix

--- src/api/popdb.py
+++ src/api/popdb.py
@@ -5,7 +5,7 @@ from api.models import Recipes, db
 
 with app.app_context(): 
     recipes = []
-    with open("./recipes.json", "rt") as recipefile: 
+    with open("./src/api/recipes.json", "rt") as recipefile: 
         recipes = json.loads(recipefile.read())
     recipes = [Recipes(**recipe) for recipe in recipes]
     db.session.add_all(recipes)

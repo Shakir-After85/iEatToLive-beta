c80ed074cb7c162b1bd9943dc590ac0a922deb4c
Shakir-After85 <shakir.tamar@gmail.com>
2023-01-05 23:00:54 +0000

    added favorite fetch

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -34,7 +34,45 @@ const getState = ({ getStore, getActions, setStore }) => {
       },
       //fetching Data from backend
       getFavoritesData: () => {
-        let user;
+        let userObj = JSON.parse(sessionStorage.getItem("iEatToLive_user"));
+
+        fetch(`${base_url}/api/list_favorites/user/${userObj.id}`)
+          .then((resp) => resp.json())
+          .then((data) => {
+            setStore({ Data: data });
+          })
+          .catch((error) => console.log("Error message from backend", error));
+      },
+      addFavorite: (recipes) => {
+        let userObj = JSON.parse(sessionStorage.getItem("iEatToLive_user"));
+        let user_id = userObj["id"];
+        let recipes_id = recipes["id"];
+
+        let payload = {
+          recipes: recipes,
+        };
+        console.log("Payload: ", payload);
+
+        return fetch(`${base_url}/api/list_favorites/user/${user_id}`, {
+          method: "PUT",
+
+          // headers: {
+          // 	"Content-Type": "application/json"
+          // },
+          body: JSON.stringify(payload),
+        })
+          .then((res) => {
+            // if (!res.ok) throw new Error(res.statusText);
+            return res.json();
+          })
+          .then((data) =>
+            setStore({
+              user: {
+                ...data.user,
+                loggedIn: true,
+              },
+            })
+          );
       },
     },
   };
